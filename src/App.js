b9b32162cd93a5693df849a877a6c3832777a1c0
CaesarHp <hp88502225@gmail.com>
2021-10-05 23:32:42 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import "./App.css";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" exact>
+            Home
+          </Route>
+          <Route path="/category" exact>
+            Category
+          </Route>
+
+          <Route path="/cart" exact>
+            Cart
+          </Route>
+          <Route path="/myaccount" exact>
+            My Account
+          </Route>
+          <Route path="/about" exact>
+            About
+          </Route>
+          <Route path="/notfound" exact>
+            Not Found
+          </Route>
+          <Route path="*">Not Found</Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
